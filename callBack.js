5ee6c5b90edc8ddbdad03f2ddf4a8c5057234cf8
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2026-05-21 01:08:28 +0530

    Create callBack.js

---
+++ callBack.js
@@ -0,0 +1,22 @@
+// function square(n){
+    // return n * n;
+// }
+
+// function cube(n){
+    // return n * n * n;
+// }
+
+// function sumOfSquares(a, b){
+    // let square1 = square(a);
+    // let square2 = square(b);
+    // return square1 + square2;
+// }
+
+// let ans = sumOfSquares(1, 2);
+// console.log(ans);
+console.log("Hello there");
+function onDone(){
+    console.log("Stuff inside onDone");
+}
+setTimeout(onDone, 6000);
+console.log("Not a part of onDone");
